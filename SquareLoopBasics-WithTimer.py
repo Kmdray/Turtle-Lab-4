6093c58062af6ec7480a48521eb6391531a476ee
wagnerj630 <166665485+wagnerj630@users.noreply.github.com>
2024-04-14 15:32:15 -0400

    Update SquareLoopBasics-WithTimer.py
    Update SquareLoopBasics-WithTimer.py

--- SquareLoopBasics-WithTimer.py
+++ SquareLoopBasics-WithTimer.py
@@ -1,26 +1,30 @@
 import turtle
-import random
 import time
+import random
 
-screen = turtle.Screen()
-
+# Change the window size
 turtle.setup(800, 800)
+
+timer_text = turtle.Turtle()
+timer_text.hideturtle()
+
+# Local variables
+horizontal = int()
+radius = int()
+colors = ["red", "blue", "yellow", "green"]
+teams = ["1111", "2222", "3333", "4444"]
+pen_size = 1
+speed = int()
 turtle.shape('turtle')
 
 
-# Local variables
-colors = ["red", "blue", "yellow", "green"]
-teams = ["1111", "2222", "3333", "4444"]
-pen_size = int()
-speed = int()
-
-
-# Loop to draw the racers running
+# Loop to draw the raceers running
 for team in range (0, 4):
     # Define Screen Reset
-    time.sleep(1)
+    time.sleep(2)
     turtle.reset()
     pen_size = 1
+    timer_text.clear()
 
     # Display team in graphics window
     turtle.penup()
@@ -32,12 +36,16 @@ for team in range (0, 4):
     turtle.goto(0, 0)
     turtle.pendown()
 
+    start = time.time()
+    
     for count in range (0, 4):
         speed  = random.randint(1,10)
+        
         #set the pen size and team color
         turtle.color(colors[count])
         turtle.pensize(pen_size)
         turtle.speed(speed)
+        
         #moving the turtle to start
         turtle.penup()
         turtle.goto(0, -300)
@@ -56,4 +64,7 @@ for team in range (0, 4):
         turtle.forward(250)
 
         pen_size = pen_size + 4
+
+    time.time() - start
+    timer_text.write("Time: " + "%0.2f" %(time.time() - start),align="center", font=("Courier", 30,))
 
